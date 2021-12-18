b82eddb86729f9e4030e6793bb7aa970252f7cf0
= <trankimhung799@gmail.com>
2021-12-18 17:02:42 +0700

    Fix:

--- index.php
+++ index.php
@@ -1,20 +1,13 @@
 <?php
-// include_once('System/libs/Main.php');
-// include_once('System/libs/DController.php');
-// include_once('System/libs/DModel.php');
-// include_once('System/libs/Database.php');
-// include_once('System/libs/Load.php');
+include_once('System/libs/Main.php');
+include_once('System/libs/DController.php');
+include_once('System/libs/DModel.php');
+include_once('System/libs/Database.php');
+include_once('System/libs/Load.php');
 
-// spl_autoload_register(function($class) {
-//     include_once('System/libs/' . $class . '.php');
-// });
+spl_autoload_register(function($class) {
+    include_once('System/libs/' . $class . '.php');
+});
 
-// var_dump($curPageName = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1)  );
-// die();
-// include_once('App/Config/config.php');
-// $main = new Main();
-print_r($_GET);
-die();
-
-// if (empty($_SERVER['PATH_INFO'])) {
-// }
+include_once('App/Config/config.php');
+$main = new Main();
